0aa3fc466e55ebffd9f1c71e3231b78dbc859046
tummala samyu <tummala samyu@DESKTOP-2QOB21C>
2019-06-25 11:56:27 +0530

    11 modified

--- Fundamentals/src/Check11.java
+++ Fundamentals/src/Check11.java
@@ -1,7 +1,7 @@
 
 public class Check11 {
 	public static void main(String args[]) {
-		for(int i=27;i<=53;i++) {
+		for(int i=Integer.parseInt(args[0]);i<=Integer.parseInt(args[1]);i++) {
 			if(i%2==0) {
 				System.out.println(i);
 			}
